e899d4c78636a45b623d5adbb0831675b6d65aca
yanghao <yangh@peoplecommnue.com>
2021-10-26 22:40:18 +0800

    优化注释

--- src/test/java/com/yh/esdemo/EsDemoApplicationTests.java
+++ src/test/java/com/yh/esdemo/EsDemoApplicationTests.java
@@ -55,8 +55,11 @@ class EsDemoApplicationTests {
      */
     @Test
     public void addIndex() throws Exception {
+        //获取 IndicesClient ，用于创建 Index
         IndicesClient indices = restHighLevelClient.indices();
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest("zeda_customer");
+        //定义一个 CreateIndexRequest ，参数为索引的名称
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest("yh_customer");
+        //CreateIndexRequest配置相关参数 number_of_shards：分片数  number_of_replicas：每个分片的副本数
         createIndexRequest.settings(
                 Settings.builder()
                         .put("index.number_of_shards", 1)
@@ -64,11 +67,11 @@ class EsDemoApplicationTests {
         );
         Map<String, Object> name = new HashMap<>();
         name.put("type", "text");
-        name.put("analyzer", "ik_max_word");
+        name.put("analyzer", "ik_max_word");//指定分词器
         name.put("search_analyzer", "ik_smart");
         Map<String, Object> timestamp = new HashMap<>();
         timestamp.put("type", "date");
-        timestamp.put("format", "yyyy-MM-dd HH:mm:ss");
+        timestamp.put("format", "yyyy-MM-dd HH:mm:ss");//指定时间格式
         Map<String, Object> uuid = new HashMap<>();
         uuid.put("type", "keyword");
         Map<String, Object> properties = new HashMap<>();
@@ -77,11 +80,13 @@ class EsDemoApplicationTests {
         properties.put("uuid", uuid);
         Map<String, Object> mapping = new HashMap<>();
         mapping.put("properties", properties);
-        //创建mapping
+        //创建mapping  mapping可以理解为此索引中数据的数据结构
         createIndexRequest.mapping(mapping);
-        //创建索引
+        //创建索引 此方法同步返回结果  异步使用 createAsync
         CreateIndexResponse createIndexResponse = indices.create(createIndexRequest, RequestOptions.DEFAULT);
+        //创建结果（true/false）
         System.out.println(createIndexResponse.isAcknowledged());
+        //输出创建成功的索引名称
         System.out.println(createIndexResponse.index());
     }
 
@@ -93,7 +98,7 @@ class EsDemoApplicationTests {
     @Test
     public void delIndex() throws Exception {
         IndicesClient indices = restHighLevelClient.indices();
-        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("zeda_customer");
+        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("yh_customer");
         AcknowledgedResponse deleteIndexResponse = indices.delete(deleteIndexRequest, RequestOptions.DEFAULT);
         System.out.println(deleteIndexResponse.isAcknowledged());
     }
@@ -109,9 +114,10 @@ class EsDemoApplicationTests {
         String timestamp = simpleDateFormat.format(new Date());
         Map<String, Object> jsonMap = new HashMap<>();
         jsonMap.put("uuid", "534523dqw4qweq24324");
-        jsonMap.put("name", "杨皓");
+        jsonMap.put("name", "张三");
         jsonMap.put("timestamp", timestamp);
-        IndexRequest indexRequest = new IndexRequest("zeda_customer").id("1").source(jsonMap);
+        //创建 IndexRequest 指定id
+        IndexRequest indexRequest = new IndexRequest("yh_customer").id("1").source(jsonMap);
         IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
         System.out.println(indexResponse);
     }
@@ -122,12 +128,13 @@ class EsDemoApplicationTests {
      */
     @Test
     public void bulkTest() {
+        //批量添加使用 BulkRequest
         BulkRequest bulkRequest = new BulkRequest();
         for (int i = 0; i < 100; i++) {
             Customer customer = new Customer(UUID.randomUUID().toString().replace("-", ""), i + "号客户", LocalDateTime.now());
             //String data = JSONObject.toJSONString(customer);
             String data = JSONObject.toJSONStringWithDateFormat(customer, "yyyy-MM-dd HH:mm:ss");
-            IndexRequest request = new IndexRequest("zeda_customer").source(data, XContentType.JSON);
+            IndexRequest request = new IndexRequest("yh_customer").source(data, XContentType.JSON);
             bulkRequest.add(request);
         }
         //同步
@@ -146,16 +153,16 @@ class EsDemoApplicationTests {
 
         //异步
         restHighLevelClient.bulkAsync(bulkRequest, RequestOptions.DEFAULT, new ActionListener<BulkResponse>() {
+            //成功回调
             @Override
             public void onResponse(BulkResponse bulkItemResponses) {
-                boolean b = bulkItemResponses.hasFailures();
-                if (b) {
-                    System.out.println(bulkItemResponses.buildFailureMessage());
-                }
+                System.out.println("success");
             }
 
+            //失败回调
             @Override
             public void onFailure(Exception e) {
+                System.out.println("failure");
                 e.printStackTrace();
             }
         });
@@ -174,9 +181,9 @@ class EsDemoApplicationTests {
      */
     @Test
     public void updateDoc() throws Exception {
-        UpdateRequest updateRequest = new UpdateRequest("zeda_customer", "1");
+        UpdateRequest updateRequest = new UpdateRequest("yh_customer", "1");
         Map<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("name", "杨皓大帅哥");
+        jsonMap.put("name", "李四");
         updateRequest.doc(jsonMap);
         UpdateResponse update = restHighLevelClient.update(updateRequest, RequestOptions.DEFAULT);
         System.out.println(update);
@@ -189,7 +196,7 @@ class EsDemoApplicationTests {
      */
     @Test
     public void exists() throws Exception {
-        GetRequest getRequest = new GetRequest("zeda_customer");
+        GetRequest getRequest = new GetRequest("yh_customer");
         getRequest.id("1");
         boolean exists = restHighLevelClient.exists(getRequest, RequestOptions.DEFAULT);
         System.out.println(exists);
@@ -202,7 +209,7 @@ class EsDemoApplicationTests {
      */
     @Test
     public void get() throws Exception {
-        GetRequest getRequest = new GetRequest("zeda_customer");
+        GetRequest getRequest = new GetRequest("yh_customer");
         getRequest.id("1");
         GetResponse getResponse = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);
         System.out.println(getResponse);
@@ -216,7 +223,7 @@ class EsDemoApplicationTests {
      */
     @Test
     public void getSource() throws Exception {
-        GetSourceRequest getRequest = new GetSourceRequest("zeda_customer", "1");
+        GetSourceRequest getRequest = new GetSourceRequest("yh_customer", "1");
         GetSourceResponse source = restHighLevelClient.getSource(getRequest, RequestOptions.DEFAULT);
         System.out.println(source);
     }
@@ -228,11 +235,13 @@ class EsDemoApplicationTests {
      */
     @Test
     public void search() throws Exception {
-        SearchRequest searchRequest = new SearchRequest("zeda_customer");//没有参数，查询所有索引
+        SearchRequest searchRequest = new SearchRequest("yh_customer");//没有参数，查询所有索引
+        //构建搜索条件
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         searchSourceBuilder.query(QueryBuilders.matchQuery("name", "1"));
         searchRequest.source(searchSourceBuilder);
         SearchResponse search = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        //返回命中数据
         SearchHits hits = search.getHits();
         for (SearchHit hit : hits.getHits()) {
             String sourceAsString = hit.getSourceAsString();
